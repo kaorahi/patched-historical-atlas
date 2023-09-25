9e0bf05d80768d5eb3841dfbed6b8ac546368155
Hiraoka <kakkokakko@gmail.com>
2023-09-25 21:58:19 +0900

    modify label language of dropdown for foreign users' safety

--- js/LangButton.js
+++ js/LangButton.js
@@ -3,19 +3,9 @@
 function LangButton()
 {
 	let on_changed_handler = null;
-	const label_table = {
-		ja: {ja: '日本語', en: '英語', zh: '中国語'},
-		en: {ja: 'Japanese', en: 'English', zh: 'Chinese'},
-		zh: {ja: '日文', en: '英文', zh: '中文'},
-	};
 
 	function update()
 	{
-		const label = label_table[data.lang];
-		Object.keys(label).forEach(lang => {
-			const option = document.querySelector(`#lang-select option[value="${lang}"]`);
-			option.innerText = label[lang];
-		});
 		if (on_changed_handler) {
 			on_changed_handler();
 		}
